dbe49d8da2f4de7cd38350d838601da34f640366
Unknown <arseneaultjoel@gmail.com>
2019-03-07 17:08:51 -0500

    Add rotated image db

--- __main__.py
+++ __main__.py
@@ -15,15 +15,6 @@ if PY3:
 import numpy as np
 import cv2 as cv
 
-# from glob import glob
-# import cv2
-# pngs = glob('./*.png')
-#
-# for j in pngs:
-#     img = cv2.imread(j)
-#     cv2.imwrite(j[:-3] + 'jpg', img)
-# tval = 27
-
 def angle_cos(p0, p1, p2):
     d1, d2 = (p0-p1).astype('float'), (p2-p1).astype('float')
     return abs( np.dot(d1, d2) / np.sqrt( np.dot(d1, d1)*np.dot(d2, d2) ) )
@@ -48,22 +39,7 @@ def find_squares(img):
             max_cos = np.max([angle_cos( cnt[i], cnt[(i+1) % 4], cnt[(i+2) % 4] ) for i in xrange(4)])
             if max_cos < 0.1:
                 squares.append(cnt)
-    # for gray in cv.split(img):
-    #     for thrs in xrange(0, 255, 26):
-    #         if thrs == 0:
-    #             bin = cv.Canny(threah_img, 0, 50, apertureSize=5)
-    #             bin = cv.dilate(bin, None)
-    #         else:
-    #             _retval, bin = cv.threshold(gray, thrs, 255, cv.THRESH_BINARY)
-    #             contours, _hierarchy = cv.findContours(bin, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
-    #             for cnt in contours:
-    #                 cnt_len = cv.arcLength(cnt, True)
-    #                 cnt = cv.approxPolyDP(cnt, 0.02*cnt_len, True)
-    #                 if len(cnt) == 4 and cv.contourArea(cnt) > 1000 and cv.isContourConvex(cnt):
-    #                     cnt = cnt.reshape(-1, 2)
-    #                     max_cos = np.max([angle_cos( cnt[i], cnt[(i+1) % 4], cnt[(i+2) % 4] ) for i in xrange(4)])
-    #                     if max_cos < 0.1:
-    #                         squares.append(cnt)
+
     return squares
 
 def find_outer_bounds(square):
@@ -83,39 +59,37 @@ def find_outer_bounds(square):
             minY = square[edge][1]
     return [maxX, maxY, minX, minY]
 
-# def convert_to_png():
-#
-#     images = []
-#     for dirpath, dirs, files in os.walk("../BDImages/"):
-#         for filename in files:
-#             full_path_name = os.path.join(dirpath,filename)
-#             if full_path_name.lower().endswith(('jpg','jpeg')):
-#
-# def load_images():
-# 	images_and_types = []
-# 	for dirpath, dirs, files in os.walk("../BDImages/"):
-# 		for filename in files:
-# 			full_path_name = os.path.join(dirpath, filename)
-# 			if full_path_name.lower().endswith(('jpg', 'png', 'jpeg', 'bmp')):
-# 				# relative path and type (folder name)
-# 				# dirpath is used as classification label
-# 				classification = dirpath.split('\\')[1]
-# 				classification = classification[:3] + (classification[-3:])[:1]
-# 				images_and_types.append([full_path_name, classification])
-# 	return images_and_types
 
+def convert_to_png():
 
+    from glob import glob
+    jpgs = glob('../**/*.jpg', recursive=True)
+    for j in jpgs:
+        img = cv.imread(j)
+        cv.imwrite(j[:-3] + 'png', img)
+
+def rotate_image(imgpath):
+    img = cv.imread(imgpath, 0)
+    rows, cols = img.shape
+
+    M = cv.getRotationMatrix2D((cols / 2, rows / 2), 90, 1)
+    dst = cv.warpAffine(img, M, (cols, rows))
 
 if __name__ == '__main__':
 
     from glob import glob
-    # for fn in glob('../data/pic*.png'):
-    # jpgs = glob('../**/*.jpg', recursive=True)
-    # for j in pngs:
-    #     img = cv2.imread(j)
-    #     cv2.imwrite(j[:-3] + 'png', img)
 
-    for fn in glob('C:/Users/joel/PycharmProjects/image_cropper/image_cropper/testImg/*.png'):
+    for fn in glob('./images/**/*.png', recursive=True):
+        # i = 90
+        # while(i<360):
+        #     img = cv.imread(fn,0)
+        #     rows, cols = img.shape
+        #
+        #     M = cv.getRotationMatrix2D((cols / 2, rows / 2), i, 1)
+        #     dst = cv.warpAffine(img, M, (cols, rows))
+        #     cv.imwrite(fn[:-4] + '-' + str(i) + '.png', dst)
+        #     i += 90
+
         img = cv.imread(fn)
         # img2 = img
         squares = find_squares(img)
@@ -125,12 +99,12 @@ if __name__ == '__main__':
         if ch == 27:
             break
 
-        # for square in  squares:
-        #     edges = find_outer_bounds(square)
-        #     img2 = img[edges[3]:edges[1], edges[2]:edges[0]]
-        #     # cv.imshow('squares',img[edges[0]:edges[2], edges[1]:[edges[3]]])
-        #     cv.imshow('squares', img2)
-        #     ch = cv.waitKey()
-        #     if ch == 27:
-        #         break
+        for square in  squares:
+            edges = find_outer_bounds(square)
+            img2 = img[edges[3]:edges[1], edges[2]:edges[0]]
+            # cv.imshow('squares',img[edges[0]:edges[2], edges[1]:[edges[3]]])
+            cv.imshow('squares', img2)
+            ch = cv.waitKey()
+            if ch == 27:
+                break
     cv.destroyAllWindows()
